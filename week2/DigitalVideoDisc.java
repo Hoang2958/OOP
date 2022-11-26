9c5eb4472e3d4b7e4d3f98321bcb33a4b633aaa2
Hoang2958 <109327730+Hoang2958@users.noreply.github.com>
2022-11-26 20:22:12 +0700

    Add files via upload

---
+++ week2/DigitalVideoDisc.java
@@ -0,0 +1,67 @@
+package week2;
+
+public class DigitalVideoDisc {
+	private String title;
+    private int id;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+	public DigitalVideoDisc(String title) {
+		super();
+		this.title = title;
+	}
+	public DigitalVideoDisc(String title,String category,float cost) {
+		this.title=title;
+		this.category=category;
+		this.cost=cost;
+	}
+	public DigitalVideoDisc(String title, String category, String director,float cost) {
+		this.title=title;
+		this.category=category;
+		this.director=director;
+		this.cost=cost;
+	}
+	public DigitalVideoDisc(String title, String category, String director,int length, float cost) {
+		this.title=title;
+		this.category=category;
+		this.director=director;
+		this.length=length;
+		this.cost=cost;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public int getId() {
+	        return id;
+	    }
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	public void setTitle(String title) {
+      this.title = title;
+  }
+	public boolean equals(Object obj){
+      DigitalVideoDisc disc = (DigitalVideoDisc)obj;
+      return this.title.equals(disc.title) && this.category.equals(disc.category)
+              && this.director.equals(disc.director) && this.length == disc.length
+              && this.cost == disc.cost;
+  }
+  public String toString() {
+      return this.id + ". DVD - " + this.title + " - " + this.category + " - " 
+      		+ this.director + " - " + this.length + ": " + this.cost + " $";
+  }
+
+  public boolean isMatch(String title) {
+      return this.title.equals(title);
+  }
+}
